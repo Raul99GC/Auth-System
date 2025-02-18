2505af7260df93b873aebe1f9eae902f88ff9ab3
Raul Cordoba <raulc99g@gmail.com>
2025-02-17 22:01:53 -0600

    Add user service interface with register user method

---
+++ server/src/main/java/com/raulcg/auth/services/user/IUserService.java
@@ -0,0 +1,8 @@
+package com.raulcg.auth.services.user;
+
+import com.raulcg.auth.models.User;
+import com.raulcg.auth.requires.CreateUserRequire;
+
+public interface IUserService {
+    User RegisterUser(CreateUserRequire user);
+}
