26158d1d4db37c51a76179901c4d1683e2a26654
Raul Cordoba <raulc99g@gmail.com>
2025-02-18 21:11:41 -0600

    Configure BCryptPasswordEncoder bean

--- server/src/main/java/com/raulcg/auth/security/SecurityConfig.java
+++ server/src/main/java/com/raulcg/auth/security/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.raulcg.auth.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
+
+    // BCryptPasswordEncoder
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
