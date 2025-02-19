e0c5f09a2f702537ad9d22f8a59a4ad81cbe2874
Raul Cordoba <raulc99g@gmail.com>
2025-02-19 16:20:56 -0600

    Rename method in RoleRepository from findByRole to findByName for consistency

--- server/src/main/java/com/raulcg/auth/repositories/RoleRepository.java
+++ server/src/main/java/com/raulcg/auth/repositories/RoleRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
-    Optional<Role> findByRole(UserRole roleName);
+    Optional<Role> findByName(UserRole roleName);
 }
