ce1df35c7b4bddc357d302e6232a73448c3bb535
Raul Cordoba <raulc99g@gmail.com>
2025-02-18 21:12:39 -0600

    Add findByRole method

--- server/src/main/java/com/raulcg/auth/repositories/RoleRepository.java
+++ server/src/main/java/com/raulcg/auth/repositories/RoleRepository.java
@@ -1,9 +1,12 @@
 package com.raulcg.auth.repositories;
 
+import com.raulcg.auth.enums.UserRole;
 import com.raulcg.auth.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository extends JpaRepository<Role, UUID> {
+    Optional<Role> findByRole(UserRole roleName);
 }
