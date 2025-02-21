1089e19f77f01d05935a9795a2b5bea10917a68d
Raul Cordoba <raulc99g@gmail.com>
2025-02-20 23:50:23 -0600

    Configure SecurityFilterChain to apply default CORS settings default CORS configuration class

--- server/src/main/java/com/raulcg/auth/security/SecurityConfig.java
+++ server/src/main/java/com/raulcg/auth/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.raulcg.auth.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,6 +21,8 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.cors(Customizer.withDefaults());
 
         http.sessionManagement(
                 managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
