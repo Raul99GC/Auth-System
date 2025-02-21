bf0c0bdf37e1816f50dffe72cabe599496c9c4d4
Raul Cordoba <raulc99g@gmail.com>
2025-02-21 15:08:51 -0600

    Add activate-account endpoint for user account activation

--- server/src/main/java/com/raulcg/auth/controllers/AuthController.java
+++ server/src/main/java/com/raulcg/auth/controllers/AuthController.java
@@ -1,9 +1,12 @@
 package com.raulcg.auth.controllers;
 
-import com.raulcg.auth.models.User;
+import com.raulcg.auth.requires.ActivateAccountRequest;
 import com.raulcg.auth.requires.CreateUserRequire;
+import com.raulcg.auth.response.GenericResponse;
 import com.raulcg.auth.response.SignupResponse;
+import com.raulcg.auth.services.accountValidationToken.IAccountValidationTokenService;
 import com.raulcg.auth.services.user.IUserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final IUserService userService;
+    private final IAccountValidationTokenService accountValidationTokenService;
 
-    public AuthController(IUserService userService) {
+    public AuthController(IUserService userService, IAccountValidationTokenService accountValidationTokenService) {
         this.userService = userService;
+        this.accountValidationTokenService = accountValidationTokenService;
     }
 
     @PostMapping("/signup")
@@ -28,4 +33,15 @@ public class AuthController {
 
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/activate-account")
+    public ResponseEntity<GenericResponse<?>> activateAccount(@Validated @RequestBody ActivateAccountRequest token) {
+        boolean isValid = accountValidationTokenService.validateToken(token);
+        if (!isValid) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new GenericResponse<>(null, "Invalid token", false));
+        }
+        return ResponseEntity.ok(new GenericResponse<>(null, "Account activated", true));
+
+    }
+
 }
