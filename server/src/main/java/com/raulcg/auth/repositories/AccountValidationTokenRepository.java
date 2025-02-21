a71a7de3b212d59f6a77a4f834fd0c8907d479c5
Raul Cordoba <raulc99g@gmail.com>
2025-02-21 14:36:05 -0600

    Add findByUser method

--- server/src/main/java/com/raulcg/auth/repositories/AccountValidationTokenRepository.java
+++ server/src/main/java/com/raulcg/auth/repositories/AccountValidationTokenRepository.java
@@ -1,9 +1,11 @@
 package com.raulcg.auth.repositories;
 
 import com.raulcg.auth.models.AccountValidationToken;
+import com.raulcg.auth.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AccountValidationTokenRepository extends JpaRepository<AccountValidationToken, UUID> {
+    AccountValidationToken findByUser(User user);
 }
