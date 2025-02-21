fb678ab359b362350d10ec4b824bbd6f59a5ab1f
Raul Cordoba <raulc99g@gmail.com>
2025-02-21 15:04:22 -0600

    Add findByEmail query method

--- server/src/main/java/com/raulcg/auth/repositories/UserRepository.java
+++ server/src/main/java/com/raulcg/auth/repositories/UserRepository.java
@@ -3,9 +3,11 @@ package com.raulcg.auth.repositories;
 import com.raulcg.auth.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
