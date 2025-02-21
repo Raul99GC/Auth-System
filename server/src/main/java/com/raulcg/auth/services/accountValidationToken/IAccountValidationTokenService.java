95f2363d8135751ff22762dcffdf3c4141a6fd01
Raul Cordoba <raulc99g@gmail.com>
2025-02-21 15:05:08 -0600

    Add validateToken method

--- server/src/main/java/com/raulcg/auth/services/accountValidationToken/IAccountValidationTokenService.java
+++ server/src/main/java/com/raulcg/auth/services/accountValidationToken/IAccountValidationTokenService.java
@@ -2,7 +2,10 @@ package com.raulcg.auth.services.accountValidationToken;
 
 import com.raulcg.auth.models.AccountValidationToken;
 import com.raulcg.auth.models.User;
+import com.raulcg.auth.requires.ActivateAccountRequest;
 
 public interface IAccountValidationTokenService {
     AccountValidationToken createToken(User user, String token);
+
+    boolean validateToken(ActivateAccountRequest request);
 }
