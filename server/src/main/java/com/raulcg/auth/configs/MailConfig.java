8043c09b78c93f1a7c70c98756e2b89ff737108d
Raul Cordoba <raulc99g@gmail.com>
2025-02-20 23:30:02 -0600

    Implement MailConfig for email sending functionality

---
+++ server/src/main/java/com/raulcg/auth/configs/MailConfig.java
@@ -0,0 +1,45 @@
+package com.raulcg.auth.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration("mailConfig")
+public class MailConfig {
+
+    @Value("${spring.mail.host}")
+    private String MAIL_HOST;
+
+    @Value("${spring.mail.port}")
+    private int MAIL_PORT;
+
+    @Value("${spring.mail.protocol}")
+    private String MAIL_PROTOCOL;
+
+    @Value("${spring.mail.username}")
+    private String MAIL_USERNAME;
+
+    @Value("${spring.mail.password}")
+    private String MAIL_PASSWORD;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(MAIL_HOST);
+        mailSender.setPort(MAIL_PORT);
+        mailSender.setUsername(MAIL_USERNAME);
+        mailSender.setPassword(MAIL_PASSWORD);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", MAIL_PROTOCOL);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
