bae39b3603a639cb1620f619d5dcb26bfa56bf3f
Raul Cordoba <raulc99g@gmail.com>
2025-02-24 17:46:44 -0600

    Create UserDetailsImpl for user auth

---
+++ server/src/main/java/com/raulcg/auth/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.raulcg.auth.security.service;
+
+import com.raulcg.auth.models.User;
+import com.raulcg.auth.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return UserDetailsImpl.build(user);
+    }
+}
