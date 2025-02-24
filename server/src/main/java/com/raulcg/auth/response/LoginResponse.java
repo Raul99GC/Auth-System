f25cb75bc92ba61aec933a8834f568719b099e46
Raul Cordoba <raulc99g@gmail.com>
2025-02-24 17:48:05 -0600

    Create LoginResponse DTO

---
+++ server/src/main/java/com/raulcg/auth/response/LoginResponse.java
@@ -0,0 +1,23 @@
+package com.raulcg.auth.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+
+    private String jwtToken;
+    private String refreshToken;
+    boolean status;
+    String message;
+
+    public LoginResponse(String jwtToken, String refreshToken) {
+        this.jwtToken = jwtToken;
+        this.refreshToken = refreshToken;
+    }
+}
