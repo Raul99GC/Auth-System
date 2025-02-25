b4d5213356587c3e5a487da66ea12c077a78b287
Raul Cordoba <raulc99g@gmail.com>
2025-02-24 23:27:50 -0600

    Enhance SecurityConfig adding JwtTokenFilter in securityFilterChain and configuring authenticationManager

--- server/src/main/java/com/raulcg/auth/security/SecurityConfig.java
+++ server/src/main/java/com/raulcg/auth/security/SecurityConfig.java
@@ -1,17 +1,27 @@
 package com.raulcg.auth.security;
 
+import com.raulcg.auth.security.jwt.AuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final AuthTokenFilter authenticationJwtTokenFilter;
+
+    public SecurityConfig(AuthTokenFilter authenticationJwtTokenFilter) {
+        this.authenticationJwtTokenFilter = authenticationJwtTokenFilter;
+    }
 
     // BCryptPasswordEncoder
     @Bean
@@ -33,8 +43,15 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
         );
 
+        http.addFilterBefore(authenticationJwtTokenFilter, BasicAuthenticationFilter.class);
+
         http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 }
