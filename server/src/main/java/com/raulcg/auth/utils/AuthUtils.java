6d28b1e6255d09a5955f96c55eb4663dc1662b39
Raul Cordoba <raulc99g@gmail.com>
2025-02-25 13:39:48 -0600

    Create AuthUtils component to get the current user logged information

---
+++ server/src/main/java/com/raulcg/auth/utils/AuthUtils.java
@@ -0,0 +1,26 @@
+package com.raulcg.auth.utils;
+
+import com.raulcg.auth.models.User;
+import com.raulcg.auth.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuthUtils {
+
+    private final UserRepository userRepository;
+
+    public AuthUtils(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> loggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return userRepository.findByUsername(username);
+    }
+
+}
