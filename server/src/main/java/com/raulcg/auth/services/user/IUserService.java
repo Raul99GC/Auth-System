934174b73dd7824961389a34b12a95cdcb0e60ed
Raul Cordoba <raulc99g@gmail.com>
2025-02-24 23:18:23 -0600

    Define findByEmail and getUserSecret methods

--- server/src/main/java/com/raulcg/auth/services/user/IUserService.java
+++ server/src/main/java/com/raulcg/auth/services/user/IUserService.java
@@ -3,10 +3,16 @@ package com.raulcg.auth.services.user;
 import com.raulcg.auth.models.User;
 import com.raulcg.auth.requires.CreateUserRequire;
 
+import java.util.Optional;
+
 public interface IUserService {
     User RegisterUser(CreateUserRequire user);
 
     User createUser(User user);
 
     boolean existUserByEmail(String mail);
+
+    Optional<User> findByEmail(String email);
+
+    String getUserSecret(String email);
 }
