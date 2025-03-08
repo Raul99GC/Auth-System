00008351c48dfab6d1711263145b15aefd142247
Raul Cordoba <raulc99g@gmail.com>
2025-03-08 14:47:59 -0600

    Create PasswordResetToken entity with ManyToOne relation to User

---
+++ server/src/main/java/com/raulcg/auth/models/PasswordResetToken.java
@@ -0,0 +1,37 @@
+package com.raulcg.auth.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class PasswordResetToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+
+    private boolean used = false;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public PasswordResetToken(String token, Instant expiryDate, User user) {
+        this.token = token;
+        this.expiryDate = expiryDate;
+        this.user = user;
+    }
+
+}
