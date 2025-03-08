5eec4e52b4fe0eef4cee1aecbe4259488d75920e
Raul Cordoba <raulc99g@gmail.com>
2025-03-08 14:52:09 -0600

    Implement sendPasswordResetEmail with email sending logic

--- server/src/main/java/com/raulcg/auth/services/email/EmailService.java
+++ server/src/main/java/com/raulcg/auth/services/email/EmailService.java
@@ -39,4 +39,25 @@ public class EmailService implements IEmailService {
             throw new EmailSendingException("No se pudo enviar el email de confirmación. "+ e.getMessage());
         }
     }
+
+    @Override
+    public void sendPasswordResetEmail(String to, String userName, String resetUrl)  {
+
+        try {
+            Context context = new Context();
+            context.setVariable("userName", userName);
+            context.setVariable("resetUrl", resetUrl);
+            String emailContent = thymeleafEngine.process("reset-password-email", context);
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject("Confirm your email address");
+            helper.setText(emailContent, true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            //TODO: Implement exception handling with logging or something else
+
+            throw new EmailSendingException("No se pudo enviar el email de confirmación. "+ e.getMessage());
+        }
+    }
 }
