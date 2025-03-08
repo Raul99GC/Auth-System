12622f30578536c8cd3361c13dcfd1f8c1b81ab0
Raul Cordoba <raulc99g@gmail.com>
2025-03-08 14:51:35 -0600

    Add sendPasswordResetEmail method

--- server/src/main/java/com/raulcg/auth/services/email/IEmailService.java
+++ server/src/main/java/com/raulcg/auth/services/email/IEmailService.java
@@ -4,4 +4,6 @@ import jakarta.mail.MessagingException;
 
 public interface IEmailService {
     void sendConfirmationEmail(String to, String userName, String authCode) throws MessagingException;
+
+    void sendPasswordResetEmail(String to, String userName, String resetUrl) throws MessagingException;
 }
