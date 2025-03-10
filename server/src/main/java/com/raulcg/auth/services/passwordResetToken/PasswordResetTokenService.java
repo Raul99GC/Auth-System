16ffc6266d14f13df09fad7e8662b685fc17cbfe
Raul Cordoba <raulc99g@gmail.com>
2025-03-10 15:00:29 -0600

    Update dependency to IEmailService and add resetPassword business logic with email dispatch

--- server/src/main/java/com/raulcg/auth/services/passwordResetToken/PasswordResetTokenService.java
+++ server/src/main/java/com/raulcg/auth/services/passwordResetToken/PasswordResetTokenService.java
@@ -1,12 +1,14 @@
 package com.raulcg.auth.services.passwordResetToken;
 
+import com.raulcg.auth.exceptions.PasswordResetTokenNotFoundException;
 import com.raulcg.auth.exceptions.UserNotFoundException;
 import com.raulcg.auth.models.PasswordResetToken;
 import com.raulcg.auth.models.User;
 import com.raulcg.auth.repositories.PasswordResetTokenRepository;
 import com.raulcg.auth.repositories.UserRepository;
-import com.raulcg.auth.services.email.EmailService;
+import com.raulcg.auth.services.email.IEmailService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -16,17 +18,19 @@ import java.util.UUID;
 @Service
 public class PasswordResetTokenService implements IPasswordResetTokenService {
 
+    private final PasswordEncoder passwordEncoder;
     @Value("${frontend.url}")
     private String frontendUrl;
 
     private final PasswordResetTokenRepository passwordResetTokenRepository;
     private final UserRepository userRepository;
-    private final EmailService emailService;
+    private final IEmailService emailService;
 
-    public PasswordResetTokenService(PasswordResetTokenRepository passwordResetTokenRepository, UserRepository userRepository, EmailService emailService) {
+    public PasswordResetTokenService(PasswordResetTokenRepository passwordResetTokenRepository, UserRepository userRepository, IEmailService emailService, PasswordEncoder passwordEncoder) {
         this.passwordResetTokenRepository = passwordResetTokenRepository;
         this.userRepository = userRepository;
         this.emailService = emailService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -41,5 +45,23 @@ public class PasswordResetTokenService implements IPasswordResetTokenService {
 
         emailService.sendPasswordResetEmail(user.getEmail(), user.getUsername(), resetUrl);
         passwordResetTokenRepository.save(resetToken);
+    }
+
+    @Override
+    public void resetPassword(String token, String password) {
+        PasswordResetToken resetToken = passwordResetTokenRepository.findByToken(token)
+                .orElseThrow(() -> new PasswordResetTokenNotFoundException("Invalid token"));
+
+        if (resetToken.isUsed() || resetToken.getExpiryDate().isBefore(Instant.now())) {
+            throw new PasswordResetTokenNotFoundException("Invalid token");
+        }
+
+        User user = resetToken.getUser();
+        user.setPassword(passwordEncoder.encode(password));
+        userRepository.save(user);
+        resetToken.setUsed(true);
+        passwordResetTokenRepository.save(resetToken);
+        emailService.sendPasswordChangedEmail(user.getEmail(), user.getUsername());
+
     }
 }
