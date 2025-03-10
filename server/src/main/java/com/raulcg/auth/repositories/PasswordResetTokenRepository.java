d16ed58f57003ecde5968c4b2286726af0ac8b8d
Raul Cordoba <raulc99g@gmail.com>
2025-03-10 14:56:48 -0600

    Add findByToken method

--- server/src/main/java/com/raulcg/auth/repositories/PasswordResetTokenRepository.java
+++ server/src/main/java/com/raulcg/auth/repositories/PasswordResetTokenRepository.java
@@ -3,5 +3,8 @@ package com.raulcg.auth.repositories;
 import com.raulcg.auth.models.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, String> {
+    Optional<PasswordResetToken> findByToken(String token);
 }
