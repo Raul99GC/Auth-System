38653308e0f2f32239248f1fd6e3dff95776c9f7
Raul Cordoba <raulc99g@gmail.com>
2025-03-10 14:58:30 -0600

    Add resetPassword method for token-based resets

--- server/src/main/java/com/raulcg/auth/services/passwordResetToken/IPasswordResetTokenService.java
+++ server/src/main/java/com/raulcg/auth/services/passwordResetToken/IPasswordResetTokenService.java
@@ -2,4 +2,5 @@ package com.raulcg.auth.services.passwordResetToken;
 
 public interface IPasswordResetTokenService {
     void generateRefreshToken(String email);
+    void resetPassword(String token, String password);
 }
