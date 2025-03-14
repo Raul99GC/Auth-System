5e1bcd222db4f2ff7a2c67d5cee8d9edf401f064
Raul Cordoba <raulc99g@gmail.com>
2025-03-14 16:51:13 -0600

    Refactor JwtUtils to implement IJwtService and method getClaimsWithoutSignature method
    Use getClaimsWithoutSignature in JWT filter to replace getKidFromToken logic

--- server/src/main/java/com/raulcg/auth/security/jwt/services/JwtService.java
+++ server/src/main/java/com/raulcg/auth/security/jwt/services/JwtService.java
@@ -1,7 +1,6 @@
-package com.raulcg.auth.security.jwt;
+package com.raulcg.auth.security.jwt.services;
 
-import com.nimbusds.jose.shaded.gson.JsonObject;
-import com.nimbusds.jose.shaded.gson.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.raulcg.auth.security.service.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
@@ -9,16 +8,18 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
-@Component
-public class JwtUtils {
+@Service
+public class JwtService implements IJwtService {
 
+    private final ObjectMapper objectMapper;
     @Value("${spring.app.jwtHeader}")
     private String jwtHeader;
 
@@ -27,6 +28,11 @@ public class JwtUtils {
 
     private int jwtExpirationMs = 7200000;
 
+    public JwtService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
     public String getJwtFromHeader(HttpServletRequest request) {
         String bearerToken = request.getHeader(jwtHeader);
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
@@ -34,6 +40,7 @@ public class JwtUtils {
         }
         return null;
     }
+
 
     private SecretKey key(String userSecret) {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(userSecret + this.jwtSecret));
@@ -48,14 +55,17 @@ public class JwtUtils {
 
     }
 
+    @Override
     public String getSubFromToken(String jwtToken, String userSecret) {
         return String.valueOf(Objects.requireNonNull(getClaimsFromToken(jwtToken, userSecret)).get("sub"));
     }
 
+    @Override
     public List<String> getAuthorities(String jwtToken, String userSecret) {
         return getClaimsFromToken(jwtToken, userSecret).get("authorities", List.class);
     }
 
+    @Override
     public String generateTokenFromUserDetails(UserDetailsImpl userDetails) {
         String email = userDetails.getEmail();
         String username = userDetails.getUsername();
@@ -75,17 +85,8 @@ public class JwtUtils {
 
         return token.compact();
     }
-
-    public String getKidFromToken(String token) {
-        String[] parts = token.split("\\.");
-        if (parts.length < 2) {
-            throw new IllegalArgumentException("Token inválido");
-        }
-        String headerJson = new String(Base64.getUrlDecoder().decode(parts[0]));
-        JsonObject header = JsonParser.parseString(headerJson).getAsJsonObject();
-        return header.get("kid").getAsString();
-    }
-
+    
+    @Override
     public boolean validateToken(String jwtToken, String userSecret) {
         try {
             Jwts.parser().verifyWith((SecretKey) key(userSecret)).build().parseSignedClaims(jwtToken);
@@ -99,5 +100,24 @@ public class JwtUtils {
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("JWT claims string is empty", e);
         }
+    }
+
+    @Override
+    public Map<String, Object> getClaimsWithoutSignature(String jwt) throws Exception {
+        // Separamos el token en partes
+        String[] partes = jwt.split("\\.");
+        if (partes.length < 2) {
+            throw new IllegalArgumentException("Token JWT inválido");
+        }
+
+        // Decodificamos el payload (la segunda parte)
+        byte[] decodedBytes = Base64.getUrlDecoder().decode(partes[1]);
+        String payload = new String(decodedBytes);
+
+        // Parseamos el JSON del payload a un Map
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> claims = mapper.readValue(payload, Map.class);
+
+        return claims;
     }
 }
