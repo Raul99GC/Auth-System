21a61cf7c6fbe68a7045c981bb83dbcf93410154
Raul Cordoba <raulc99g@gmail.com>
2025-03-17 20:37:22 -0600

    Mark UserMapper as a component for reuse in various services

--- server/src/main/java/com/raulcg/auth/mapper/UserMapper.java
+++ server/src/main/java/com/raulcg/auth/mapper/UserMapper.java
@@ -3,9 +3,11 @@ package com.raulcg.auth.mapper;
 import com.raulcg.auth.models.Role;
 import com.raulcg.auth.models.User;
 import com.raulcg.auth.response.UserResponse;
+import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
 
+@Component
 public class UserMapper {
 
     public static UserResponse mapToUserResponse(User user) {
